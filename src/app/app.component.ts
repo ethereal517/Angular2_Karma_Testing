bfcd018cb1ebdde240da8ce4dddbd5a65acf6322
SmartGuy <adam.lindberg520@gmx.com>
2017-03-01 14:22:08 +0100

    app.component logic completed with test spec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 import { TodoDataService } from './todo-data.service';
 
 @Component({
@@ -8,11 +9,27 @@ import { TodoDataService } from './todo-data.service';
   providers: [TodoDataService]
 })
 export class AppComponent {
+
+  newTodo: Todo = new Todo();
+
   constructor(private todoDataService: TodoDataService) {
 
   }
 
+  addTodo() {
+  	this.todoDataService.addTodo(this.newTodo);
+  	this.newTodo = new Todo();
+  }
+
   toggleTodoComplete(todo) {
   	this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+  	this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+  	return this.todoDataService.getAllTodos();
   }
 }
