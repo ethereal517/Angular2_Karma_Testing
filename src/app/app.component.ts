06596af05d8f64251b4f6eefb386fade0645b1f3
SmartGuy <adam.lindberg520@gmx.com>
2017-03-01 13:54:32 +0100

    app.component initialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private todoDataService: TodoDataService) {
+
+  }
+
+  toggleTodoComplete(todo) {
+  	this.todoDataService.toggleTodoComplete(todo);
+  }
 }
